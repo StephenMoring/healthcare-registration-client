1e046f1b19727f463b0781cf45e5a4fed2215529
Stephen Moring <stephenmoring@stephens-mbp.attlocal.net>
2020-07-23 01:22:19 -0400

    added material ui to project, added a navbar component, started registration component, started playing with background image

--- src/App.js
+++ src/App.js
@@ -1,24 +1,16 @@
-import React from 'react';
-import logo from './logo.svg';
+import React, { useState } from 'react';
 import './App.css';
+import Registration from './components/auth/Registration';
+import DashboardNavBar from './components/DashboardNavBar';
 
 function App() {
+  const [test, setTest] = useState(0);
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <DashboardNavBar></DashboardNavBar>
+      <div class="background">
+        <Registration></Registration>
+      </div>
     </div>
   );
 }
---
+++ src/components/auth/Registration.js
@@ -0,0 +1,14 @@
+import React, { useState } from 'react';
+import Button from '@material-ui/core/Button'; 
+import './Registration.css';
+
+function Registration(props){
+    return(
+        <div class="centered">
+            <Button variant="contained" color="primary">  
+                Material UI  
+            </Button>  
+        </div>
+    )
+}
+export default Registration
